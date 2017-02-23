0b8607e4518e17ecbb433e92d533499d6b203590
djdapz <djdapz@aol.com>
2017-02-23 16:14:30 -0600

    asd

--- bucketLib.py
+++ bucketLib.py
@@ -143,9 +143,9 @@ def buildOutMissingValues(buckets):
             last_hr = bucket.heart_rate
         else:
             if last_hr > resting_heart_rate:
-                bucket.heart_rate = last_hr - int((last_hr - resting_heart_rate)/2)
+                bucket.heart_rate = last_hr - (last_hr - resting_heart_rate)/2
             else:
-                bucket.heart_rate = last_hr + int((resting_heart_rate - last_hr) / 2)
+                bucket.heart_rate = last_hr + (resting_heart_rate - last_hr) / 2
 
         #
         # if bucket.hr_max:
