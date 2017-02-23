3225f9095ad15cfca6a253d95f3c78d440b028c1
djdapz <djdapz@aol.com>
2017-02-23 03:40:30 -0600

    asd

--- mvpaGateway.py
+++ mvpaGateway.py
@@ -63,11 +63,11 @@ elif os.environ['REQUEST_METHOD'] == 'GET':
                 print "<p>got buckets...<p>"
                 print "<table>"
                 buckets[0].printTableHeader()
-                # step_sum = 0
-                # calories_sum = 0
+                step_sum = 0
+                calories_sum = 0
                 for bucket in buckets:
-                    # step_sum = bucket.steps + step_sum
-                    # calories_sum = bucket.sum + calories_sum
+                    step_sum = bucket.steps + step_sum
+                    calories_sum = bucket.calories + calories_sum
                     bucket.printTableRow()
                 print "</table>"
 
