c951e8640818ed486fcf32a4e41d00d42d88a786
djdapz <djdapz@aol.com>
2017-02-24 14:16:37 -0600

    asd

--- mvpaGateway.py
+++ mvpaGateway.py
@@ -187,7 +187,14 @@ elif os.environ['REQUEST_METHOD'] == 'GET':
                     objects = []
                     for bucket in buckets:
                         x = {}
-                        x['mvpa'] = mvpa_sum
+                        x['mvpa_guess'] = str(bucket.mvpa_guess)
+                        x['heart_rate'] = str(bucket.heart_rate)
+                        x['hr_max'] = str(bucket.hr_max)
+                        x['hr_min'] = str(bucket.hr_min)
+                        x['calories'] = str(bucket.calories)
+                        x['steps'] = str(bucket.steps)
+                        x['start_time'] = str(bucket.start_time)
+                        x['end_time'] = str(bucket.end_time)
                         objects.append(x)
                     print (json.JSONEncoder().encode(objects))
 
