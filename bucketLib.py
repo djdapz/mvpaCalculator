e37285420a2e86e526558d2f40799f54c2b71697
djdapz <djdapz@aol.com>
2017-02-28 14:15:52 -0600

    asdf

--- bucketLib.py
+++ bucketLib.py
@@ -197,10 +197,11 @@ def getBuckets(start_time, end_time, uid, db):
 
     #todo get uid
     sql_query = "select distinct * from "+db+" where uid = '"+ uid+"';"
+    print sql_query
     cur.execute(sql_query)
     rows = cur.fetchall()
 
-
+    print 'EXECUTED'
 
 
     interval = 5  # IN MINUTES
