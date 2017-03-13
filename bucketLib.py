a678e8d313f59ef9bd67a9b8b7145a2e1fc6efb7
djdapz <djdapz@aol.com>
2017-03-13 12:48:28 -0500

    scale

--- bucketLib.py
+++ bucketLib.py
@@ -112,6 +112,7 @@ class Bucket:
 
 
 def scaleBuckets(buckets, scale):
+    print here
     for bucket in buckets:
         bucket.calories = bucket.calories / scale
         bucket.steps = bucket.steps / scale
